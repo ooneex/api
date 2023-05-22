c7e2cfdf1edd223938352ab603c236d5cd10b1a6
fkdiomande <fkdiomande.dev@gmail.com>
2023-05-22 21:45:17 +0300

    fix(controller): use Request interface

--- controllers/HomepageController.ts
+++ controllers/HomepageController.ts
@@ -1,10 +1,11 @@
 import { Route } from "@ooneex/decorator";
-import { Request, Response as HttpResponse } from "@ooneex/http";
+import type { IRequest } from "@ooneex/http";
+import { Response as HttpResponse } from "@ooneex/http";
 
 export class HomepageController {
   @Route("homepage", "/")
   public index(
-    _request: Request,
+    _request: IRequest,
     response: HttpResponse,
   ): Response {
     return response.json({ message: "Welcome" });
